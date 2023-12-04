984987e6aae118f121dec942e91e50eada3203ea
Pathe SENE <pathe.sene@gmail.com>
2023-12-04 22:05:22 +0000

    add more test hands

--- src/hand_test.rs
+++ src/hand_test.rs
@@ -21,3 +21,33 @@ pub fn bad_hand_color(color: CardColor) -> Vec<Card> {
         Card::new(CardColor::CLUBS, CardFamily::KING),
     ]
 }
+
+pub fn two_of_family(family: CardFamily) -> Vec<Card> {
+    return vec![
+        Card::new(CardColor::CLUBS, family),
+        Card::new(CardColor::HEARTS, family),
+        Card::new(CardColor::CLUBS, CardFamily::TEN),
+        Card::new(CardColor::CLUBS, CardFamily::QUEEN),
+        Card::new(CardColor::CLUBS, CardFamily::KING),
+    ];
+}
+
+pub fn three_of_family(family: CardFamily) -> Vec<Card> {
+    return vec![
+        Card::new(CardColor::CLUBS, family),
+        Card::new(CardColor::HEARTS, family),
+        Card::new(CardColor::CLUBS, family),
+        Card::new(CardColor::CLUBS, CardFamily::QUEEN),
+        Card::new(CardColor::CLUBS, CardFamily::KING),
+    ];
+}
+
+pub fn four_of_family(family: CardFamily) -> Vec<Card> {
+    return vec![
+        Card::new(CardColor::CLUBS, family),
+        Card::new(CardColor::HEARTS, family),
+        Card::new(CardColor::CLUBS, family),
+        Card::new(CardColor::CLUBS, family),
+        Card::new(CardColor::CLUBS, CardFamily::KING),
+    ];
+}
